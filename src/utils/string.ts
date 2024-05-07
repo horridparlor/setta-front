faefe1d3fddd45a60b28a020f39d252345769acd
Eero Laine <horridparlormaster@gmail.com>
2024-05-08 02:05:59 +0300

    Added dash to not normalize

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -4,12 +4,12 @@ export const normalizeName = (name: any): string => {
     }
     return name.replace(/^({i}The)/i, '{i}')
         .replace(/{[^}]*}/g, '')
-        .replace(/[^a-zA-Z0-9\s']/g, '');
+        .replace(/[^a-zA-Z0-9\s'-]/g, '');
 };
 
 export const serializeName = (name: any): string => {
     return normalizeName(name)
-        .replace(/'/g, '')
+        .replace(/'-/g, '')
         .split(' ')
         .map(word => word.charAt(0).toUpperCase() + word.slice(1))
         .join(' ')
