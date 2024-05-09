3df698e6b34a4ea12b349b06c0feb3b4250b30d3
Eero Laine <horridparlormaster@gmail.com>
2024-05-09 17:51:42 +0300

    Changed ctrl a to ctrl l

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ const App: React.FC<AppProps> = () => {
                         event.preventDefault();
                         commitExport();
                         break;
-                    case 'a':
+                    case 'l':
                         homeBarRef.current?.toggleLoginOpen();
                         break;
 
