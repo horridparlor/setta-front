d4905a25553cd28e18c103c66e3c229c08478a57
Eero Laine <horridparlormaster@gmail.com>
2024-06-25 21:07:41 +0300

    Started designing imporved effects typing

--- src/types/cardEffects.tsx
+++ src/types/cardEffects.tsx
@@ -1,79 +1,97 @@
-import {CardClass, CardSubtype, CardSupertype, CardType} from "./card";
-
-export enum TriggerType {
-    AUTOMATIC = 'Automatic',
-    CONTINUOUS = 'Continuous',
-    PAID = 'Paid',
-}
-
-export enum AutomaticTriggerType {
-    FLIPPED_WHEN_ATTACKED = 'Flipped when attacked',
-}
-
-export enum PaymentType {
+export enum CostType {
     DISCARD = 'Discard',
-    MILL = 'Mill'
-}
-
-export enum CardStat {
-    ATK = 'Atk',
-    DEF = 'Def',
-    EFFECTS = 'Effects',
-    LIFE = 'Life',
-    LEVEL = 'Level'
-}
-
-export enum Player {
-    BOTH = 'Both',
-    OPPONENT = 'Opponent',
-    YOU = 'You'
-}
-
-export enum Zone {
-    ARMOR_STACK = 'Armor stack',
-    DECK = 'Deck',
-    EXILE = 'EXILE',
-    EXTRA_DECK = 'Extra-deck',
-    GRAVE = 'Grave',
-    HAND = 'Hand',
-    PARASITE = 'Parasite'
+    FLIPPED_WHEN_ATTACKED = 'Flipped when attacked',
+    NONE = 'None',
+    PAY_LIFE = 'Pay life',
+    YOU_CONTROL_ONLY_THIS = 'You control only this',
 }
 
 export enum EffectType {
-    KEYWORD = 'Keyword',
+    EXTRAMILL = 'Extramill',
+    CANNOT_ATTACK_DIRECTLY = 'Cannot attack directly',
+    DESTROY = 'Destroy',
+    MILL = 'Mill',
+    REBORN = 'Reborn',
+    RETRIEVE = 'Retrieve',
     STAT = 'Stat',
 }
 
 export enum EffectRelation {
+    ANY_POSITION = 'Any position',
+    ATTACK_POSITION = 'In attack',
+    DEFENSE_POSITION = 'In defense',
     GAIN = 'Gain',
-    LOSE = 'Lose'
+    LOSE = 'Lose',
 }
 
-export enum Keyword {
-    GOADED = 'Goaded',
-    INCINERATION = 'Incineration',
-    PIERCING = 'Piercing'
+export enum TargetType {
+    ALL_CARDS = 'All cards',
+    ALL_MONSTERS = 'All monsters',
+    BOTH_PLAYERS = 'Both players',
+    OPPONENT = 'Opponent',
+    OPPONENTS_MONSTERS = 'Opponent\'s monsters',
+    TARGET_MONSTER = 'Target monster',
+    THIS = 'This',
+    YOU = 'You',
+    YOUR_MONSTERS = 'Your monsters',
 }
 
-export interface CardEffectsTarget {
-    player: Player|null;
-    stat: CardStat|null;
-    amount: number|null;
-    cardClass: CardClass|null;
-    cardType: CardType|null;
-    cardSubtype: CardSubtype|null;
-    cardSupertype: CardSupertype|null;
+export interface EffectsCost {
+    costType: CostType;
+    amount: number;
 }
 
-export interface CardEffectsEffect {
-    effectType: EffectType|null;
+export enum CardTag {
+    LIFE_GAIN = 'Life-gain',
+    TURN_ENDING = 'Turn ending',
+}
+
+export enum FilterType {
+    ATK = 'Atk',
+    CARD = 'Card',
+    CARD_CLASS = 'Class',
+    CARD_SUBTYPE = 'Subtype',
+    CARD_SUPERTYPE = 'Supertype',
+    CARD_TAG = 'Tag',
+    CARD_TYPE = 'Type',
+    DEF = 'Def',
+    LEVEL = 'Level',
+    LIFE = 'Life',
+}
+
+export interface TargetFilter {
+    filterType: FilterType;
+    included: Array<number|string>;
+    max: number|null;
+    min: number|null;
+    notIncluded: Array<number|string>
+}
+
+export interface EffectTarget {
+    targetType: TargetType;
+    filters: Array<TargetFilter>;
+}
+
+export enum CountType {
+    CARDS_IN_GRAVE = 'Cards in grave',
+
+}
+
+export interface CountedAmount {
+    countType: CountType;
+    target: EffectTarget;
+}
+
+export interface EffectsEffect {
+    effectType: EffectType;
     relation: EffectRelation|null;
     amount: number|null;
-    target: CardEffectsTarget
+    countedAmount : CountedAmount|null;
+    target: EffectTarget;
+    chainedEffect: EffectsEffect|null;
 }
 
 export interface CardEffects {
-    cost: CardEffectsCost|null;
-    effect: CardEffectsEffect|null;
-    chain: CardEffectsEffect|null;
+    cost: EffectsCost;
+    effect: EffectsEffect;
 }
