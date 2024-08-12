a92edec29a968ac6be6248a4ec4fadd69e5fd4cb
Eero Laine <horridparlormaster@gmail.com>
2024-08-12 15:26:56 +0300

    Instead lets just fix the effects when entering the tab

--- src/components/card-editor/RightPanelSettings.tsx
+++ src/components/card-editor/RightPanelSettings.tsx
@@ -924,11 +924,17 @@ const RightPanelSettings: React.FC<RightPanelSettingsProps> = ({
         );
     }
 
+    const fixEffects = () => {
+        if (!getCostTypeOptions(cardData).includes(getEffectsCost(cardData).costType)) {
+            handleCostTypeChange(getDefaultCostType(cardData));
+        }
+    }
+
     return (
         <Box sx={{ bgcolor: 'background.paper', p: 2 }}>
             <Tabs value={tabId} onChange={onTabClicked} aria-label="form tabs" sx={{marginBottom: '2rem', marginTop: '2rem'}}>
                 <Tab label="Stats" />
-                <Tab label="Effects" onClick={() => handleSelectChange('cardType')({ target: { value: cardData.cardType } } as SelectChangeEvent<string>)} />
+                <Tab label="Effects" onClick={() => fixEffects()} />
             </Tabs>
             <Box sx={{display: tabId === STATS_TAB ? 'block' : 'none'}}>
                 <Box
