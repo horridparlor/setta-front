cc7aa4bb0c9b2f042f21334d90e0ae0a091c4289
Pertsaa <65128229+Pertsaa@users.noreply.github.com>
2024-09-19 17:16:12 +0300

    feat: language select component

---
+++ src/components/common/LanguageSelect.tsx
@@ -0,0 +1,21 @@
+import { MenuItem, Select, SelectChangeEvent } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+
+export default function LanguageSelect() {
+  const { i18n } = useTranslation();
+
+  const handleChange = async (e: SelectChangeEvent) => {
+    await i18n.changeLanguage(e.target.value);
+  };
+
+  return (
+    <Select
+      value={i18n.language}
+      onChange={handleChange}
+      sx={{ backgroundColor: 'white', marginLeft: '0.4rem' }}
+    >
+      <MenuItem value="fi">Finnish</MenuItem>
+      <MenuItem value="en">English</MenuItem>
+    </Select>
+  );
+}
