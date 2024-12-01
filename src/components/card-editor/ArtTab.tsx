34749d8a09792ed9a7a9a27ae0e46c8b567b17e2
Heini Vuorela <heini.vuorela@tuni.fi>
2024-12-01 10:14:20 +0200

    Move add new style to its own modal

--- src/components/card-editor/ArtTab.tsx
+++ src/components/card-editor/ArtTab.tsx
@@ -6,7 +6,6 @@ import {
   TextField,
   Typography,
   Button,
-  Modal,
   IconButton,
   InputAdornment,
 } from '@mui/material';
@@ -67,12 +66,6 @@ const ArtTab = () => {
   const updatePromptValues = (newValues: Partial<typeof promptValues>) => {
     setPromptValues(prevValues => ({ ...prevValues, ...newValues }));
   };
-
-  // Modal State and Functions
-  const [openStyleModal, setOpenStyleModal] = useState(false);
-
-  const handleOpenStyleModal = () => setOpenStyleModal(true);
-  const handleCloseStyleModal = () => setOpenStyleModal(false);
 
   const handleSubTabChange = (
     _event: React.SyntheticEvent,
@@ -139,31 +132,6 @@ const ArtTab = () => {
       <Box sx={{ display: subTabId === 2 ? 'block' : 'none' }}>
         <SpecialEffectsTab updatePromptValues={updatePromptValues} />
       </Box>
-
-      {/* Style Modal Button */}
-      <Button
-        variant="contained"
-        onClick={handleOpenStyleModal}
-        sx={{ marginTop: 2 }}
-      >
-        Add New Style
-      </Button>
-
-      {/* Modal for Styles */}
-      <Modal open={openStyleModal} onClose={handleCloseStyleModal}>
-        <Box
-          sx={{
-            padding: 4,
-            backgroundColor: 'white',
-            margin: 'auto',
-            maxWidth: '500px',
-          }}
-        >
-          <Typography variant="h6">Create or Edit Style</Typography>
-          {/* Additional form fields for Style creation to be implemented later */}
-          <Button onClick={handleCloseStyleModal}>Close</Button>
-        </Box>
-      </Modal>
 
       {/* Edit Mode Toggle Button */}
       <Box
