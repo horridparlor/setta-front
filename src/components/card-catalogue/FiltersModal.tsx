4dc2b8b753968450782e890090539b272ad70bd8
Eero Laine <horridparlormaster@gmail.com>
2024-12-19 01:55:46 +0200

    Removed redundant import

--- src/components/card-catalogue/FiltersModal.tsx
+++ src/components/card-catalogue/FiltersModal.tsx
@@ -27,7 +27,6 @@ import {
 } from '../../types/card';
 import { ComparisonType, SortOption, SortOrder } from '../../types/filter';
 import { CardOwner } from '../../types/user';
-import { getUser } from '../../types/api';
 
 interface FiltersModalProps {
   isOpen: boolean;
@@ -296,9 +295,6 @@ const FiltersModal: React.FC<FiltersModalProps> = ({
           <Grid item xs={12} sm={6} md={3}></Grid>
           <Grid item xs={12} sm={6} md={3}>
             <Button
-              sx={{
-                display: getUser().accessRights.isSuperAdmin ? 'flex' : 'none',
-              }}
               onClick={handleExportAll}
               variant="contained"
               color="secondary"
