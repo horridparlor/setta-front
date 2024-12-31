f338db185a8b4ef7a704200d8343c3f34ae3b4d5
Eero Laine <horridparlormaster@gmail.com>
2024-12-31 05:11:35 +0200

    Add labels for strike and assist

--- src/components/card-editor/card-previewer/StatFrame.tsx
+++ src/components/card-editor/card-previewer/StatFrame.tsx
@@ -11,24 +11,38 @@ interface StatFrameProps {
 }
 
 const StatFrame: React.FC<StatFrameProps> = ({ value, statType, scale }) => (
-  <Typography
-    variant="h6"
-    sx={{
-      textAlign: 'center',
-      backgroundColor: getStatBoxBackgroundColor(statType),
-      borderRadius: `${0.8 * scale}rem`,
-      color: TextColor.WHITE,
-      padding: `${0.1 * scale}rem`,
-      paddingTop: `${0.4 * scale}rem`,
-      fontSize: getFontSize(8, scale),
-      fontWeight: '700',
-      width: `${7.07 * scale}rem`,
-      height: `${4.5 * scale}rem`,
-      border: `solid ${0.2 * scale}rem black`,
-    }}
-  >
-    {value}
-  </Typography>
+  <>
+    <Typography
+      variant="h6"
+      sx={{
+        textAlign: 'center',
+        backgroundColor: getStatBoxBackgroundColor(statType),
+        borderRadius: `${0.8 * scale}rem`,
+        color: TextColor.WHITE,
+        padding: `${0.1 * scale}rem`,
+        paddingBottom: `${0.4 * scale}rem`,
+        fontSize: getFontSize(8, scale),
+        fontWeight: '700',
+        width: `${7.07 * scale}rem`,
+        height: `${4.5 * scale}rem`,
+        border: `solid ${0.2 * scale}rem black`,
+      }}
+    >
+      {value}
+    </Typography>
+    <Typography
+      sx={{
+        textAlign: 'center',
+        marginTop: `-${1.9 * scale}rem`,
+        fontSize: getFontSize(0, scale),
+        fontWeight: '600',
+        opacity: 0.34,
+        color: 'white',
+      }}
+    >
+      {statType == StatType.ATTACK ? 'STRIKE' : 'ASSIST'}
+    </Typography>
+  </>
 );
 
 export default StatFrame;
